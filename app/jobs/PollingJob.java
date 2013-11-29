d9407c02dfba6c0a2fd9cb742296d97721e925cb
k-shunji <k-shunji@flect.co.jp>
2013-11-29 11:52:24 +0900

    PollingJob

--- app/jobs/PollingJob.java
+++ app/jobs/PollingJob.java
@@ -4,15 +4,17 @@ import play.jobs.Job;
 import play.jobs.Every;
 import play.libs.WS;
 
-@Every("30min")
-public class PollingJob {
+@Every("1min")
+public class PollingJob extends Job {
 	
 	public void doJob() {
 		String url = System.getenv("POLLING_URL");
+System.out.println("URL: " + url);
 		if (url != null) {
 			try {
 				WS.url(url).get();
 			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
     }
