9ced682c34867d114360bc16111ebbc1e1964200
k-shunji <k-shunji@flect.co.jp>
2013-11-29 16:25:26 +0900

    Remove println

--- app/jobs/PollingJob.java
+++ app/jobs/PollingJob.java
@@ -9,7 +9,6 @@ public class PollingJob extends Job {
 	
 	public void doJob() {
 		String url = System.getenv("POLLING_URL");
-System.out.println("URL: " + url);
 		if (url != null) {
 			try {
 				WS.url(url).get();
